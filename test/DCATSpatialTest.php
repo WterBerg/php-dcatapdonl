d74065d5ce30efad55c56db2d159c63d8a0bd985
Willem <wrpterberg@gmail.com>
2018-09-10 00:13:21 +0200

    basic testcase for spatial

--- test/DCATSpatialTest.php
+++ test/DCATSpatialTest.php
@@ -1,10 +1,23 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use DCAT_AP_DONL\DCATSpatial;
 
 
 class DCATSpatialTest extends TestCase {
 
+    public function testBothSchemeAndValueAreRequired(): void
+    {
+        $spatial = new DCATSpatial();
 
+        $this->assertEquals(
+            [
+                'Spatial: scheme is missing',
+                'Spatial: value is missing',
+                'Spatial: at least one property must be provided'
+            ],
+            $spatial->validate()->getMessages()
+        );
+    }
 
 }
