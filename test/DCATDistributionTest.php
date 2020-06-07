5132aac1eff6f8c4f3f0780099f1293d2e25961b
Willem ter Berg <wrpterberg@gmail.com>
2020-06-07 18:18:15 +0200

    introduced additional unit-tests for DCATDistribution

--- test/DCATDistributionTest.php
+++ test/DCATDistributionTest.php
@@ -2,13 +2,80 @@
 
 namespace DCAT_AP_DONL\Test;
 
+use DCAT_AP_DONL\DCATChecksum;
+use DCAT_AP_DONL\DCATControlledVocabularyEntry;
+use DCAT_AP_DONL\DCATDateTime;
 use DCAT_AP_DONL\DCATDistribution;
 use DCAT_AP_DONL\DCATException;
 use DCAT_AP_DONL\DCATLiteral;
+use DCAT_AP_DONL\DCATNumber;
+use DCAT_AP_DONL\DCATURI;
 use PHPUnit\Framework\TestCase;
 
 class DCATDistributionTest extends TestCase
 {
+    public function testGettersAndSettersArePresentAndFunctional(): void
+    {
+        try {
+            $title    = new DCATLiteral('testTitle');
+            $description = new DCATLiteral('testDescription');
+            $access_url = new DCATURI('https://test.uri');
+            $byte_size = new DCATNumber(1);
+            $format = new DCATControlledVocabularyEntry('test', 'MDR:FiletypeNAL');
+            $license = new DCATControlledVocabularyEntry('test', 'Overheid:License');
+            $mediatype = new DCATControlledVocabularyEntry('test', 'IANA:Mediatypes');
+            $language = new DCATControlledVocabularyEntry('test', 'DONL:Language');
+            $type = new DCATControlledVocabularyEntry('test', 'DONL:DistributionType');
+            $modification_date = new DCATDateTime('testDate');
+            $release_date = new DCATDateTime('testDate');
+            $rights    = new DCATLiteral('testRights');
+            $status = new DCATControlledVocabularyEntry('', 'ADMS:Distributiestatus');
+            $url = new DCATURI('https://example.com');
+            $checksum = new DCATChecksum();
+
+            $distribution = new DCATDistribution();
+            $distribution->setTitle($title);
+            $distribution->setDescription($description);
+            $distribution->setAccessURL($access_url);
+            $distribution->setByteSize($byte_size);
+            $distribution->setFormat($format);
+            $distribution->setLicense($license);
+            $distribution->setMediaType($mediatype);
+            $distribution->setMetadataLanguage($language);
+            $distribution->setDistributionType($type);
+            $distribution->setModificationDate($modification_date);
+            $distribution->setReleaseDate($release_date);
+            $distribution->setRights($rights);
+            $distribution->setStatus($status);
+            $distribution->addDocumentation($url);
+            $distribution->addDownloadURL($url);
+            $distribution->addLanguage($language);
+            $distribution->addLinkedSchema($url);
+            $distribution->setChecksum($checksum);
+
+            $this->assertEquals($title, $distribution->getTitle());
+            $this->assertEquals($description, $distribution->getDescription());
+            $this->assertEquals($access_url, $distribution->getAccessURL());
+            $this->assertEquals($byte_size, $distribution->getByteSize());
+            $this->assertEquals($format, $distribution->getFormat());
+            $this->assertEquals($license, $distribution->getLicense());
+            $this->assertEquals($mediatype, $distribution->getMediaType());
+            $this->assertEquals($language, $distribution->getMetadataLanguage());
+            $this->assertEquals($type, $distribution->getDistributionType());
+            $this->assertEquals($modification_date, $distribution->getModificationDate());
+            $this->assertEquals($release_date, $distribution->getReleaseDate());
+            $this->assertEquals($rights, $distribution->getRights());
+            $this->assertEquals($status, $distribution->getStatus());
+            $this->assertEquals([$url], $distribution->getDocumentations());
+            $this->assertEquals([$url], $distribution->getDownloadURLs());
+            $this->assertEquals([$language], $distribution->getLanguages());
+            $this->assertEquals([$url], $distribution->getLinkedSchemas());
+            $this->assertEquals($checksum, $distribution->getChecksum());
+        } catch (DCATException $e) {
+            $this->fail($e->getMessage());
+        }
+    }
+
     public function testEmptyDistributionsDoNotValidate(): void
     {
         $distribution = new DCATDistribution();
@@ -40,6 +107,90 @@ class DCATDistributionTest extends TestCase
                 'DCATDistribution::unserialize(string) is not implemented',
                 $e->getMessage()
             );
+        }
+    }
+
+    public function testChecksIfControlledVocabularyIsAcceptedForFormat(): void
+    {
+        try {
+            $dataset = new DCATDistribution();
+            $dataset->setFormat(new DCATControlledVocabularyEntry('', 'MDF:FiletypeNAL'));
+
+            $this->fail();
+        } catch (DCATException $e) {
+            $this->assertEquals('Expected a DCATControlledVocabularyEntry of vocabulary MDR:FiletypeNAL', $e->getMessage());
+        }
+    }
+
+    public function testChecksIfControlledVocabularyIsAcceptedForLicense(): void
+    {
+        try {
+            $dataset = new DCATDistribution();
+            $dataset->setLicense(new DCATControlledVocabularyEntry('', 'UnknownVocabulary'));
+
+            $this->fail();
+        } catch (DCATException $e) {
+            $this->assertEquals('Expected a DCATControlledVocabularyEntry of vocabulary Overheid:License', $e->getMessage());
+        }
+    }
+
+    public function testChecksIfControlledVocabularyIsAcceptedForLanguage(): void
+    {
+        try {
+            $dataset = new DCATDistribution();
+            $dataset->addLanguage(new DCATControlledVocabularyEntry('', 'UnknownVocabulary'));
+
+            $this->fail();
+        } catch (DCATException $e) {
+            $this->assertEquals('Expected a DCATControlledVocabularyEntry of vocabulary DONL:Language', $e->getMessage());
+        }
+    }
+
+    public function testChecksIfControlledVocabularyIsAcceptedForMetadataLanguage(): void
+    {
+        try {
+            $dataset = new DCATDistribution();
+            $dataset->setMetadataLanguage(new DCATControlledVocabularyEntry('', 'UnknownVocabulary'));
+
+            $this->fail();
+        } catch (DCATException $e) {
+            $this->assertEquals('Expected a DCATControlledVocabularyEntry of vocabulary DONL:Language', $e->getMessage());
+        }
+    }
+
+    public function testChecksIfControlledVocabularyIsAcceptedForStatus(): void
+    {
+        try {
+            $dataset = new DCATDistribution();
+            $dataset->setStatus(new DCATControlledVocabularyEntry('', 'UnknownVocabulary'));
+
+            $this->fail();
+        } catch (DCATException $e) {
+            $this->assertEquals('Expected a DCATControlledVocabularyEntry of vocabulary ADMS:Distributiestatus', $e->getMessage());
+        }
+    }
+
+    public function testChecksIfControlledVocabularyIsAcceptedForMediatype(): void
+    {
+        try {
+            $dataset = new DCATDistribution();
+            $dataset->setMediaType(new DCATControlledVocabularyEntry('', 'UnknownVocabulary'));
+
+            $this->fail();
+        } catch (DCATException $e) {
+            $this->assertEquals('Expected a DCATControlledVocabularyEntry of vocabulary IANA:Mediatypes', $e->getMessage());
+        }
+    }
+
+    public function testChecksIfControlledVocabularyIsAcceptedForType(): void
+    {
+        try {
+            $dataset = new DCATDistribution();
+            $dataset->setDistributionType(new DCATControlledVocabularyEntry('', 'UnknownVocabulary'));
+
+            $this->fail();
+        } catch (DCATException $e) {
+            $this->assertEquals('Expected a DCATControlledVocabularyEntry of vocabulary DONL:DistributionType', $e->getMessage());
         }
     }
 }
